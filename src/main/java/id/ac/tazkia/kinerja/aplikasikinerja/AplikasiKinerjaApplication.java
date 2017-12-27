0f1be8d9abe3821f11c5a714baac9b1ceb7e45fd
Endy Muhardin <endy.muhardin@gmail.com>
2017-12-27 10:54:57 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/AplikasiKinerjaApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.kinerja.aplikasikinerja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiKinerjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiKinerjaApplication.class, args);
+	}
+}
