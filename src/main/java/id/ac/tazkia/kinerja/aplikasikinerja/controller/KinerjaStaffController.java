e744662da07ae0bbf1b640962da26f00e16bff07
Sawrain <ramadhani@tazkia.ac.id>
2018-01-04 08:41:23 +0700

    Screen individual achievement score dan tazkia value score

--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/KinerjaStaffController.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/KinerjaStaffController.java
@@ -1,4 +1,15 @@
 package id.ac.tazkia.kinerja.aplikasikinerja.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/kinerjastaf")
+@Controller
 public class KinerjaStaffController {
+
+    @GetMapping("/form")
+    public void listPageStaf(){
+
+    }
 }
