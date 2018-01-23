3c8b8d19cf57de4eeaebd3374e5664bdf7693784
gifar <gifar@tazkia.ac.id>
2018-01-23 09:11:35 +0700

    login form

---
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/LoginController.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.kinerja.aplikasikinerja.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public void loginPage(){
+
+    }
+}
