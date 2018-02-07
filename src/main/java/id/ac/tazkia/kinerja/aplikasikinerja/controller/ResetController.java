386fef3219d1b286655b61266a7168f84678c3bd
gifar <gifar@tazkia.ac.id>
2018-02-07 08:27:09 +0700

    Screen lupa password

---
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/ResetController.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.kinerja.aplikasikinerja.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResetController {
+    @RequestMapping(value = "/reset")
+    public void reset(){
+
+    }
+}
