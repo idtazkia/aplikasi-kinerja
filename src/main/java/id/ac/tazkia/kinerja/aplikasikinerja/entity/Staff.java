4b9d521ac4dcb7d9021cfb83e3189bd78dd30372
gifar <gifar@tazkia.ac.id>
2018-02-12 14:27:18 +0700

    Daftar bawahan berdasarkan atasan

---
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/entity/Staff.java
@@ -0,0 +1,59 @@
+package id.ac.tazkia.kinerja.aplikasikinerja.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity @Data
+public class Staff {
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 150)
+    private String employeeName;
+
+    @Column(nullable = false)
+    @NotNull
+    @NotEmpty
+    private String employeeNumber;
+
+    @Column(nullable = false)
+    @Email
+    @NotEmpty
+    @NotNull
+    private String jobLevel;
+
+    @Column(nullable = false)
+    @NotNull
+    @NotEmpty
+    private String jobTitle;
+
+    @NotNull
+    private String department;
+
+    @Column(nullable = false)
+    @NotNull
+    @NotEmpty
+    private String idSuperior;
+
+
+    @Column(nullable = false)
+    @NotNull
+    @NotEmpty
+    private String area;
+
+    @NotNull
+    @OneToOne @JoinColumn(name = "id_user")
+    private User user;
+
+
+
+}
