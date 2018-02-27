df1d592801f67b9dbadb60a9df1466e961c55552
Sawrain <ramadhani@tazkia.ac.id>
2018-02-27 15:10:12 +0700

    Score

---
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/ScoreDao.java
@@ -0,0 +1,9 @@
+package id.ac.tazkia.kinerja.aplikasikinerja.dao;
+
+
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.Score;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ScoreDao extends PagingAndSortingRepository<Score,String> {
+
+}
