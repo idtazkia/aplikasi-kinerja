0ecbdc3d04bab586d4ed0b22bbec36948559f045
gifar <gifar@tazkia.ac.id>
2018-03-01 13:49:46 +0700

    lihat comment dari bawahan

--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/DaftarBawahanController.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/DaftarBawahanController.java
@@ -2,19 +2,32 @@ package id.ac.tazkia.kinerja.aplikasikinerja.controller;
 
 import id.ac.tazkia.kinerja.aplikasikinerja.dao.*;
 import id.ac.tazkia.kinerja.aplikasikinerja.entity.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Controller
 public class DaftarBawahanController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DaftarBawahanController.class);
 
     @Autowired
     private UserDao userDao;
@@ -22,13 +35,17 @@ public class DaftarBawahanController {
     @Autowired
     private StaffDao staffDao;
 
-    @Autowired private KpiDao kpiDao;
-
     @Autowired
     private StaffKpiDao staffKpiDao;
 
     @Autowired
     private IndicatorsDao indicatorsDao;
+
+    @Autowired
+    private ScoreDao scoreDao;
+
+    @Value("${upload.folder}")
+    private String uploadFolder;
 
     @RequestMapping("/daftarbawahan/list")
     public void  daftarStaff(Model model,String user, Authentication currentUser)throws Exception{
@@ -80,6 +97,37 @@ public class DaftarBawahanController {
     }
 
     @GetMapping("/daftarbawahan/komen")
-    public void komens(){}
+    public void komens(@RequestParam(required = true)String id, Model m){
+
+        m.addAttribute("individual",scoreDao.findByStaffKpiStaffIdAndStaffKpiKpiCategoryId(id,"001"));
+        m.addAttribute("tazkiaValue",scoreDao.findByStaffKpiStaffIdAndStaffKpiKpiCategoryId(id,"002"));
+
+
+    }
+
+    @GetMapping("/uploaded/{evidence}/bukti/")
+    public ResponseEntity<byte[]> tampilkanBuktiPembayaran(@PathVariable Evidence evidence) throws Exception {
+        String lokasiFile = uploadFolder + File.separator + evidence.getScore().getStaffKpi().getStaff().getId()
+                + File.separator + evidence.getFileName();
+        LOGGER.debug("Lokasi file bukti : {}",lokasiFile);
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            if(evidence.getFileName().toLowerCase().endsWith("jpeg") || evidence.getFileName().toLowerCase().endsWith("jpg")) {
+                headers.setContentType(MediaType.IMAGE_JPEG);
+            } else if(evidence.getFileName().toLowerCase().endsWith("png")){
+                headers.setContentType(MediaType.IMAGE_PNG);
+            }else if(evidence.getFileName().toLowerCase().endsWith("pdf")) {
+                headers.setContentType(MediaType.APPLICATION_PDF);
+            }else {
+                headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            }
+            byte[] data = Files.readAllBytes(Paths.get(lokasiFile));
+            return new ResponseEntity<byte[]>(data, headers, HttpStatus.OK);
+        } catch (Exception err){
+            LOGGER.warn(err.getMessage(), err);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
 }
