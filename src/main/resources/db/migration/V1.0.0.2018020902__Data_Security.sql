d181b4e86c9d2a21bb37ed7a665afc63fcab8577
gifar <gifar@tazkia.ac.id>
2018-03-15 08:58:23 +0700

    hapus script yang tidak perlu

--- src/main/resources/db/migration/V1.0.0.2018020902__Data_Security.sql
+++ src/main/resources/db/migration/V1.0.0.2018020902__Data_Security.sql
@@ -13,17 +13,3 @@ INSERT INTO s_role_permission (id_role, id_permission) VALUES
   ('employee', 'editemployee'),
   ('superior', 'viewsuperior'),
   ('superior', 'editsuperior');
-
--- INSERT INTO s_user (id, active, username, id_role) VALUES
---   ('001', true, 'user', 'pendaftar');
---
--- INSERT INTO s_user (id, active, username, id_role) VALUES
---   ('u002', true, 'admin', 'humas');
---
--- -- password : 123
--- INSERT INTO s_user_password (id, id_user, password) VALUES
---   ('up001', 'u001', '$2a$17$Mhfv.hlqIybDHWqAaTMU/.PKi8RDntt6xe9pTMGQLfnW3phTlhROm');
---
--- -- password : 1234
--- INSERT INTO s_user_password (id, id_user, password) VALUES
---   ('up002', 'u002', '$2a$17$g6pNPoXyIknhS1lax/zIoetaUWWTeG7tP/xV3Fpx1FCY3mjDfWnT.');
