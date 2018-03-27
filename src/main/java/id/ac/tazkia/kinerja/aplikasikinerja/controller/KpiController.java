27002b820965af97c5aeadddcfacabc618f03c81
gifar <gifar@tazkia.ac.id>
2018-03-27 08:02:36 +0700

    screen kpi list

--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/KpiController.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/KpiController.java
@@ -2,13 +2,11 @@ package id.ac.tazkia.kinerja.aplikasikinerja.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class KinerjaStaffController {
-
+public class KpiController {
     @GetMapping("/kpi/list")
-    public void listPageStaf(){
+    public void list() {
 
     }
 }
