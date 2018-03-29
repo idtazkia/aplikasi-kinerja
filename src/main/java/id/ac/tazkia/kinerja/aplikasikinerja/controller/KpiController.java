2535ebc6f5ef6d80a7e073d90e0e4997ec6ea477
gifar <gifar@tazkia.ac.id>
2018-03-29 08:41:05 +0700

    tampilkan kpi yang aktif

--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/KpiController.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/KpiController.java
@@ -1,9 +1,9 @@
 package id.ac.tazkia.kinerja.aplikasikinerja.controller;
 
 import id.ac.tazkia.kinerja.aplikasikinerja.dao.KpiDao;
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.StatusKpi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -14,13 +14,14 @@ public class KpiController {
     @Autowired
     private KpiDao kpiDao;
 
+
     @GetMapping("/kpi/list")
-    public void list( Model model,Pageable page,String keyresult) {
+    public void list(Model model, Pageable page, String keyresult) {
         if (StringUtils.hasText(keyresult)) {
             model.addAttribute("keyresult", keyresult);
-            model.addAttribute("data", kpiDao.findByKeyResultContainingIgnoreCaseOrderByKeyResult(keyresult, page));
+            model.addAttribute("data", kpiDao.findByStatusAndKeyResultContainingIgnoreCaseOrderByKeyResult(StatusKpi.AKTIF,keyresult, page));
         } else {
-            model.addAttribute("data", kpiDao.findAll(page));
+            model.addAttribute("data", kpiDao.findByStatus(StatusKpi.AKTIF,page));
         }
     }
 
@@ -28,4 +29,5 @@ public class KpiController {
     public void form() {
 
     }
+
 }
--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/KpiDao.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/KpiDao.java
@@ -1,11 +1,13 @@
 package id.ac.tazkia.kinerja.aplikasikinerja.dao;
 
 import id.ac.tazkia.kinerja.aplikasikinerja.entity.Kpi;
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.StatusKpi;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 
 public interface KpiDao extends PagingAndSortingRepository<Kpi, String> {
-    Page<Kpi> findByKeyResultContainingIgnoreCaseOrderByKeyResult(String keyresult, Pageable page);
+    Page<Kpi> findByStatusAndKeyResultContainingIgnoreCaseOrderByKeyResult(StatusKpi aktif, String keyresult, Pageable page);
+    Page<Kpi> findByStatus(StatusKpi aktif,Pageable page);
 }
