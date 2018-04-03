5d12cb47e0a0fad94237a59fe9d258f88a0f6835
Sawrain <ramadhani@tazkia.ac.id>
2018-04-03 10:12:40 +0700

    Tambahan Staff Controller

--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/StaffController.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/StaffController.java
@@ -2,14 +2,25 @@ package id.ac.tazkia.kinerja.aplikasikinerja.controller;
 
 import id.ac.tazkia.kinerja.aplikasikinerja.dao.KpiDao;
 import id.ac.tazkia.kinerja.aplikasikinerja.dao.StaffDao;
+import id.ac.tazkia.kinerja.aplikasikinerja.dao.StaffRoleDao;
+import id.ac.tazkia.kinerja.aplikasikinerja.dao.StaffRoleStaffDao;
 import id.ac.tazkia.kinerja.aplikasikinerja.entity.Kpi;
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.Staff;
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.StaffRoleStaff;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class StaffController {
@@ -18,27 +29,63 @@ public class StaffController {
     StaffDao staffDao;
 
     @Autowired
+    StaffRoleDao staffRoleDao;
+
+    @Autowired
+    StaffRoleStaffDao staffRoleStaffDao;
+
+    @Autowired
     KpiDao kpiDao;
+
+
 
     @ModelAttribute("daftarKpi")
     public Iterable<Kpi> daftarKpi() {
         return kpiDao.findAll();
     }
 
-    @GetMapping("/staff/list")
-    public void daftarStaff(@RequestParam(required = false) String staff, Model m, Pageable page) {
-        m.addAttribute("daftarStaff", staffDao.findAll());
-
-    }
-
-
-    @GetMapping("/staff/form")
-    public void formPageStaf() {
-
-    }
-
     @GetMapping("staff/kpi")
     public void staffKpi() {
+    }
+
+
+    @GetMapping("/staff/list")
+    public void daftarStaff(Model m, String staff,@PageableDefault(size = 10) Pageable page, String search ) throws Exception{
+        m.addAttribute("daftarStaff",staffDao.findAll(page));
+
+    }
+
+
+    @RequestMapping(value = "/staff/form", method = RequestMethod.GET)
+    public void tampilkanForm(Model model){
+        model.addAttribute("staff", new Staff());
+    }
+
+
+    @RequestMapping(value = "/staff/form", method = RequestMethod.POST)
+    public String prosesForm(@Valid Staff s, BindingResult errors){
+        if(errors.hasErrors()){
+            return "/staff/form";
+        }
+        staffDao.save(s);
+        return "redirect:list";
+    }
+
+
+
+    @GetMapping("/staff/role")
+    public void formPageRole(Model m2, String staffRole) throws Exception{
+        m2.addAttribute("panggilRole",staffRoleDao.findAll());
+
+    }
+
+    @RequestMapping(value = "/staff/role", method = RequestMethod.POST)
+    public String prosesFormRole(@ModelAttribute @Valid StaffRoleStaff srs, BindingResult errors){
+        if(errors.hasErrors()){
+            return "/staff/role";
+        }
+        staffRoleStaffDao.save(srs);
+        return "redirect:list";
     }
 
 }
