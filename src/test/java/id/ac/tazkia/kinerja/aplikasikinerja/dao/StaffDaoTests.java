1a45e28f702992b0b79c6f33cbe37a56da29ed5a
Sawrain <ramadhani@tazkia.ac.id>
2018-04-06 17:10:21 +0700

    aplication properties

--- src/test/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/StaffDaoTests.java
+++ src/test/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/StaffDaoTests.java
@@ -1,6 +1,7 @@
 package id.ac.tazkia.kinerja.aplikasikinerja.dao;
 
 import id.ac.tazkia.kinerja.aplikasikinerja.entity.Staff;
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.StaffRole;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -17,6 +20,8 @@ public class StaffDaoTests {
 
     @Autowired
     private StaffDao staffDao;
+
+    @Autowired private StaffRoleDao staffRoleDao;
 
     @Test
     public void testAmbilBawahan() {
@@ -33,5 +38,24 @@ public class StaffDaoTests {
         for (Staff bawahan : daftarBawahan) {
             System.out.println("Nama : " + bawahan.getEmployeeName());
         }
+    }
+
+    @Test
+    public void testAmbilRole(){
+        Staff staff = staffDao.findById("001").get();
+        for(StaffRole roles : staff.getRoles()) {
+            System.out.println("Role : "+roles.getRoleName());
+        }
+
+        Set<StaffRole> roleBaru = new HashSet<>();
+        StaffRole direktur = staffRoleDao.findById("003").get();
+        roleBaru.add(direktur);
+
+        StaffRole sekretaris = staffRoleDao.findById("006").get();
+        roleBaru.add(sekretaris);
+
+        staff.setRoles(roleBaru);
+
+        staffDao.save(staff);
     }
 }
