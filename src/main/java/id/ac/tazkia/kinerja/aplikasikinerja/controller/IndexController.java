c660c3ab43cd26542fe940c3ac18e8b6b2bc1d9a
gifar <gifar@tazkia.ac.id>
2018-04-13 11:22:47 +0700

    list periode di dashboard user

--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/IndexController.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/IndexController.java
@@ -1,13 +1,19 @@
 package id.ac.tazkia.kinerja.aplikasikinerja.controller;
 
+import id.ac.tazkia.kinerja.aplikasikinerja.dao.PeriodeDao;
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.StatusKpi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private PeriodeDao periodeDao;
 
     @GetMapping("/index")
-    public void indexPage(){
-
+    public void indexPage(Model model){
+        model.addAttribute("periode",periodeDao.findByStatusOrderByActiveAsc(StatusKpi.AKTIF));
     }
 }
