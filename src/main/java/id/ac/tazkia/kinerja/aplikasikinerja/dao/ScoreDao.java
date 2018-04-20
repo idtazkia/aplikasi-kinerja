1e13416458bbe6e7d83baeb0bdffc8ae2ad79f06
gifar <gifar@tazkia.ac.id>
2018-04-20 09:14:31 +0700

    dao search by staf,kpi dan category

--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/ScoreDao.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/ScoreDao.java
@@ -1,6 +1,7 @@
 package id.ac.tazkia.kinerja.aplikasikinerja.dao;
 
 import id.ac.tazkia.kinerja.aplikasikinerja.entity.Category;
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.Periode;
 import id.ac.tazkia.kinerja.aplikasikinerja.entity.Score;
 import id.ac.tazkia.kinerja.aplikasikinerja.entity.Staff;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -9,7 +10,7 @@ import java.util.List;
 
 
 public interface ScoreDao extends PagingAndSortingRepository<Score, String> {
-    List<Score> findByStaffAndKpiCategory(Staff s, Category c);
+    List<Score> findByStaffAndKpiCategoryAndPeriode(Staff staff, Category category, Periode periode);
 }
 
 
