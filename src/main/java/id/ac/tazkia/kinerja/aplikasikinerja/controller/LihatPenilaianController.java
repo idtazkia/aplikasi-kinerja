9f7577ca302db9fdc94fb1ecac620e52c2266094
Sawrain <ramadhani@tazkia.ac.id>
2018-04-23 16:02:48 +0700

    Page Total

--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/LihatPenilaianController.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/controller/LihatPenilaianController.java
@@ -8,15 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
@@ -108,6 +106,12 @@ public class LihatPenilaianController {
 
 
     }
+
+    @RequestMapping(value = "/lihatpenilaian/total", method = RequestMethod.GET)
+    public void tampilkanFormTotal(Model model){
+
+    }
+
 
 //    @PostMapping("/lihatpenilaian/comment")
 //    public String proses(@ModelAttribute @Valid Evidence evidence, MultipartFile fileBukti) throws Exception {
