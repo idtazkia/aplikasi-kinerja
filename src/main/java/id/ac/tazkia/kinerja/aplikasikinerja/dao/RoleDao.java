94ecea23cdecd063652010d9cf472018771f9064
gifar <gifar@tazkia.ac.id>
2018-04-23 16:10:35 +0700

    role dao

---
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/RoleDao.java
@@ -0,0 +1,7 @@
+package id.ac.tazkia.kinerja.aplikasikinerja.dao;
+
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.Role;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface RoleDao extends PagingAndSortingRepository<Role,String> {
+}
