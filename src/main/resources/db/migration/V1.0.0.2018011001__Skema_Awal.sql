59e1e954f85dd459d362e7a172658b0ef5ff1c4f
Endy Muhardin <endy.muhardin@gmail.com>
2018-05-08 17:10:40 +0700

    konsolidasi skema

--- src/main/resources/db/migration/V1.0.0.2018011001__Skema_Awal.sql
+++ src/main/resources/db/migration/V1.0.0.2018011001__Skema_Awal.sql
@@ -1,3 +1,63 @@
+CREATE TABLE s_permission (
+  id               VARCHAR(255) NOT NULL,
+  permission_label VARCHAR(255) NOT NULL,
+  permission_value VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (permission_value)
+);
+
+CREATE TABLE s_role (
+  id          VARCHAR(255) NOT NULL,
+  description VARCHAR(255) DEFAULT NULL,
+  name        VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (name)
+);
+
+CREATE TABLE s_role_permission (
+  id_role       VARCHAR(255) NOT NULL,
+  id_permission VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_role, id_permission),
+  FOREIGN KEY (id_permission) REFERENCES s_permission (id),
+  FOREIGN KEY (id_role) REFERENCES s_role (id)
+);
+
+CREATE TABLE s_user (
+  id       VARCHAR(36),
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(50) NOT NULL ,
+  active   BOOLEAN      NOT NULL,
+  id_role  VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (username),
+  FOREIGN KEY (id_role) REFERENCES s_role (id)
+);
+
+CREATE TABLE s_user_role (
+  id_user VARCHAR(36) not null,
+  id_role VARCHAR(36) NOT NULL,
+  PRIMARY KEY (id_user,id_role),
+  FOREIGN KEY (id_user) REFERENCES s_user (id),
+  FOREIGN KEY (id_role) REFERENCES s_role (id)
+);
+
+create table s_user_password (
+  id varchar(36),
+  id_user varchar(36) not null,
+  password varchar(255) not null,
+  primary key (id),
+  foreign key (id_user) references s_user (id)
+);
+
+CREATE TABLE reset_password (
+  id VARCHAR(36) NOT NULL ,
+  id_user VARCHAR(36) NOT NULL ,
+  code VARCHAR(36) NOT NULL ,
+  expired DATE NOT NULL ,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_user) REFERENCES s_user(id)
+);
+
 create table staff_role (
   id VARCHAR (255)NOT NULL ,
   role_name VARCHAR (255)NOT NULL ,
@@ -5,7 +65,6 @@ create table staff_role (
   id_role_superior VARCHAR(36),
   status VARCHAR(36),
   PRIMARY KEY (id)
-
 );
 
 CREATE TABLE staff (
@@ -17,7 +76,6 @@ CREATE TABLE staff (
   department VARCHAR (40),
   area VARCHAR (40),
   id_user VARCHAR (255),
-
   PRIMARY KEY (id)
 );
 
@@ -73,7 +131,6 @@ create table periode (
   active VARCHAR (36) NOT NULL,
   status VARCHAR(36) NOT NULL,
   PRIMARY KEY (id)
-
 );
 
 CREATE TABLE score (
@@ -101,7 +158,6 @@ CREATE TABLE evidence (
   FOREIGN KEY (id_staff) REFERENCES staff(id),
   FOREIGN KEY (id_kpi) REFERENCES kpi(id),
   FOREIGN KEY (id_periode) REFERENCES periode(id)
-
 );
 
 CREATE TABLE score_comment (
@@ -115,4 +171,15 @@ CREATE TABLE score_comment (
   FOREIGN KEY (author) REFERENCES staff(id),
   FOREIGN KEY (score) REFERENCES score(id),
   FOREIGN KEY (periode) REFERENCES periode(id)
+);
+
+CREATE TABLE master_data(
+  id VARCHAR(36)not null,
+  staff VARCHAR (36) NOT NULL ,
+  category VARCHAR (36) NOT NULL ,
+  kpi VARCHAR (36) NOT NULL ,
+  staff_kpi VARCHAR (36) NOT NULL ,
+  indicators VARCHAR (36) NOT NULL ,
+  score VARCHAR (36) NOT NULL ,
+  PRIMARY KEY (id)
 );
