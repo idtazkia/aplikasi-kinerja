3c45ff391ef7f8bd87866559d6dd8f996c5758b8
gifar <gifar@tazkia.ac.id>
2018-05-14 15:58:43 +0700

    daftarbawahan komen

--- src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/ScoreCommentDao.java
+++ src/main/java/id/ac/tazkia/kinerja/aplikasikinerja/dao/ScoreCommentDao.java
@@ -1,12 +1,12 @@
 package id.ac.tazkia.kinerja.aplikasikinerja.dao;
 
-import id.ac.tazkia.kinerja.aplikasikinerja.entity.Periode;
-import id.ac.tazkia.kinerja.aplikasikinerja.entity.Score;
-import id.ac.tazkia.kinerja.aplikasikinerja.entity.ScoreComment;
-import id.ac.tazkia.kinerja.aplikasikinerja.entity.Staff;
+import id.ac.tazkia.kinerja.aplikasikinerja.entity.*;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 
 public interface ScoreCommentDao extends PagingAndSortingRepository<ScoreComment,String> {
     ScoreComment findByAuthorAndPeriodeAndScore(Staff staff, Periode periode, Score score);
+    List<ScoreComment> findByAuthorAndPeriodeAndScoreKpiCategory(Staff staff, Periode periode, Category category);
 }
